4e6723b686d9854fd7a9f052e3a452fb064f99de
Maria Fernanda <bubu.ramos.f@gmail.com>
2025-05-27 17:13:21 -0300

    Removendo a pasta

--- victorhbbp.py
+++ victorhbbp.py
@@ -15,3 +15,16 @@ print(matriz_2)
 
 
 ##lembrar de dar (pip instal numpy) para funcionar
+
+# ATIVIDADE 3
+
+a11 = int(input('Qual seria o a11? '))
+a12 = int(input('Qual seria o a12? '))
+a21 = int(input('Qual seria o a21? '))
+a22 = int(input('Qual seria o a22? '))
+MatrizA = np.array([[a11, a12],[a21, a22]])
+Transposta = MatrizA.T
+resultado = np.dot(MatrizA, Transposta)
+determinante = np.linalg.det(resultado)
+print("Determinante do A x At:")
+print(round(determinante))
